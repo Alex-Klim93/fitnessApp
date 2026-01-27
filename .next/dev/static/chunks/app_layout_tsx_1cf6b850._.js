41e79e5d697c6285033cf2e291e61a162bdd3a4d
Alex-Klim93 <melkimelki79@gmail.com>
2026-01-27 10:28:35 +0300

    ооптлпртооо

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__30d12a9b._.css",
-  "static/chunks/node_modules_84b12029._.js",
-  "static/chunks/app_8de620ec._.js"
-],
-    source: "dynamic"
-});
